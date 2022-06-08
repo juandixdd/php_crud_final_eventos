e05a85f99ccf4586c5c89ed36913c5ba90adf7df
Juandi <juandixarteagaperez210@gmail.com>
2022-06-08 16:31:58 -0500

    Se agrega header y footer

---
+++ template/footer.php
@@ -0,0 +1,15 @@
+<footer class="container-fluid bg-dark fixed-bottom p-3">
+    <div class="row">
+        <div class="col-md text-light text-end">
+            <h5>&copy; JuanDiEvent 2022</h5>
+        </div>
+    </div>
+</footer>
+
+
+
+    <!-- ? Bootstrap script -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ template/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="assets/styles/style.css" rel="stylesheet">
+    <title>JuanDi Event</title>
+</head>
+
+<body>
+    <!-- ? headder  -->
+    <div class="container-fluid container-header">
+        <div class="row">
+            <div class="col-md">
+                <header class="p-3">
+                    <a href="#">
+                        <div class="row align-items-center">
+                            <img src="assets/img/logo.png" alt="Logo" class="logo-header" class="col">
+                            <h3 class="col">JuandiEvents</h3>
+                        </div>
+                    </a>
+                </header>
+            </div>
+        </div>
+    </div>
