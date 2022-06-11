c759e4456cae1d430600cebc13d03a2e78bb0ea6
Juandi <juandixarteagaperez210@gmail.com>
2022-06-11 12:07:47 -0500

    Se cambian estilos

--- template/header.php
+++ template/header.php
@@ -7,30 +7,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="assets/styles/style.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Courgette&display=swap" rel="stylesheet">
     <title>JuanDi Event</title>
 </head>
 
 <body>
     <!-- ? headder  -->
     <div class="container-fluid container-header">
-        <header class="p-3">
+        <header class="p-2">
             <div class="row align-items-center justify-content-between">
                 <div class="col-md-3">
                     <div class="row align-items-center">
                         <a href="#" class="col-auto"><img src="assets/img/logo.png" alt="Logo" class="logo-header "></a>
-                        <a href="#" class="col-auto"><h3>JuandiEvents</h3></a>
+                        <a href="#" class="col-auto header-tittle">
+                            <h3>JuandiEvents</h3>
+                        </a>
                     </div>
                 </div>
-                <div class="col-md-3">
-
-                </div>
-                <div class="col-md-3">
-                    <div class="row justify-content-between">
-                        <div class="col">
-                            <a href="principalPage.php" class="btn btn-outline-dark">Eventos</a>
+                <div class="col-md-3 p-2">
+                    <div class="row justify-content-center">
+                        <div class="col-auto p-1">
+                            <a href="principalPage.php" class="btn btn-outline-light width-auto">Eventos</a>
                         </div>
-                        <div class="dropdown col">
-                            <button class="btn btn-dark dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                        <div class="dropdown col-auto p-1">
+                            <button class="btn btn-custom-secundary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
                                 Juan Diego Arteaga
                             </button>
                             <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
