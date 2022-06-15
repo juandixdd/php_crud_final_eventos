af0c92dcef2e8bc1870aeffb4c396890396b4b93
Juandi <juandixarteagaperez210@gmail.com>
2022-06-15 12:10:27 -0500

    Se agregan eventos suscritos al menú del usuario

--- profile.php
+++ profile.php
@@ -1,24 +1,58 @@
 <?php include 'template/header.php'; ?>
 
+<?php
+include_once 'model/conexion.php';
+$query = $bd->prepare("select concat(e.name, ' - Semana ' ,r.semana) as 'event' from users u
+join usuario_evento r on u.id = r.id_user
+join events e on e.id = r.id_event
+where u.id = ?
+order by e.name asc
+");
+$result = $query->execute([$_SESSION['user']['id']]);
+$events = $query->fetchAll(PDO::FETCH_OBJ);
+
+
+?>
+
+
+
 <!-- ? profile -->
 
 <div class="container scale-up-center" style="margin-bottom: 100px;">
     <div class="row justify-content-center m-5">
-        <div class="col-md-10 mt-5">
+        <div class="col-md-3 mt-5">
             <div class="card">
                 <div class="card-body">
-                    <h2 class="card-title">Juan Diego Arteaga Pérez</h2>
-                    <hr>
-                    <h5><b>Edad: </b>22</h5>
-                    <h5><b>Email: </b> email@email.com </h5>
-                    <div class="row justify-content-end">
-                        <button class="btn btn-custom-primary mt-4 col-md-3">Editar datos de usuario</button>
-                    </div>
+                    <h5 class="card-title">Eventos suscritos</h5>
+               
+                    <?php foreach ($events as $event) { ?>
+                        <ul class="list-group">
+                            <li class="list-group-item"><?php echo $event->event ?></li>
+                        </ul>
+                    <?php } ?>
                 </div>
+
             </div>
         </div>
+        <div class="col-md-7 mt-5">
+            <div class="card">
+                <div class="card-body">
+                    <h2 class="card-title"><?php echo $_SESSION['user']['names'] . " " . $_SESSION['user']['last_names'] ?> </h2>
+                    <hr>
+                    <h5><b>Edad:</b> <?php echo $_SESSION['user']['age']?></h5>
+                    <h5><b>Email:</b> <?php echo $_SESSION['user']['email']?> </h5>
+                    
+                </div>
+
+
+
+
+            </div>
+        </div>
+
     </div>
-</div>
 
 
-<?php include 'template/footer.php'; ?>
+
+
+    <?php include 'template/footer.php'; ?>
